47df7aab3b186f9683a79c9486343f3c1bac0470
James Dm <jjaimedzm@gmail.com>
2022-01-07 22:48:08 -0600

    Archivos actualizados

---
+++ copy_files.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+vimFilePath="/mnt/c/Users/IsitaDev013/.vimrc"
+emacsFilePath="/home/james/.emacs.d/init.el"
+tmuxFilePath="/mnt/c/Users/IsitaDev013/.tmux.conf"
+
+echo "${vimFilePath}"
+echo "${emacsFilePath}"
+echo "${tmuxFilePath}"
+
+cp $vimFilePath ./vim/.vimrc
+resposneVimFile=$?
+# echo "${resposneVimFile}"
+cp $emacsFilePath ./emacs/init.el
+responseEmacsFile=$?
+# echo "$?"
+cp $tmuxFilePath ./tmux/.tmux.conf
+responseTmuxFile=$?
+# echo "$?"
+
+response=$(($resposneVimFile + $responseEmacsFile + $responseTmuxFile))
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo $response
+
+if [ $response -gt 0 ] 
+then
+  git add .
+  git commit -m "Archivos actualizados"
+  git push origin main
+  echo "Los archivos han sido agregados al repositorio"
+else
+  echo "Nada que agregar :D"
+fi
