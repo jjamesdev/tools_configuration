1a946b275e763a1101ef2bdac7c64d9b9758c060
James Dm <jjaimedzm@gmail.com>
2022-01-07 22:49:54 -0600

    Archivos actualizados

--- emacs/init.el
+++ emacs/init.el
@@ -1,1 +1,118 @@
-/home/james/.emacs.d/init.el
+(setq org-refile-use-outline-path 'file) (require 'package)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+;(setq org-agenda-files (list "~/Dropbox/emacs/web_developer.org"
+;                             "~/org/casa.org"))
+
+
+;(custom-set-variables
+ ;;; custom-set-variables was added by Custom.
+ ;;; If you edit it by hand, you could mess it up, so be careful.
+ ;;; Your init file should contain only one such instance.
+ ;;; If there is more than one, they won't work right.
+ ;'(package-selected-packages '(evil)))
+;(custom-set-faces
+ ;;; custom-set-faces was added by Custom.
+ ;;; If you edit it by hand, you could mess it up, so be careful.
+ ;;; Your init file should contain only one such instance.
+ ;;; If there is more than one, they won't work right.
+ ;)
+
+;(require 'evil)
+;(evil-mode 1)
+
+; wrap text
+(add-hook 'text-mode-hook 'visual-line-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(org-agenda-files (quote ("/mnt/c/Users/IsitaDev013/Dropbox/emacs"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+			
+;; Identacion en modo org
+;;(setq org-startup-indented t)
+
+
+;; remap
+;;(define-key 'global-map (kbd "C-.") 'set-mark-command)
+;;(global-set-key (kbd "C-.") 'set-mark-command)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CONFIGURACION articulo de Org mode ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; mandar un TODO a otro archivo
+;;(setq org-refile-use-outline-path 'file)
+(setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
+
+(setq org-capture-templates
+       `(("i" "Inbox" entry  (file "/mnt/c/Users/IsitaDev013/Dropbox/emacs/inbox.org")
+        ,(concat "* TODO %?\n"
+                 "/Entered on/ %U"))))(setq org-capture-templates
+       `(("i" "Inbox" entry  (file "/mnt/c/Users/IsitaDev013/Dropbox/emacs/inbox.org")
+        ,(concat "* TODO %?\n"
+                 "/Entered on/ %U"))))
+
+
+(defun org-capture-inbox ()
+     (interactive)
+     (call-interactively 'org-store-link)
+     (org-capture nil "i"))
+
+(define-key global-map (kbd "C-c i") 'org-capture-inbox)
+
+;; org capture
+(define-key global-map (kbd "C-c c") 'org-capture)
+
+
+(setq org-agenda-hide-tags-regexp ".")
+
+(setq org-agenda-prefix-format
+      '((agenda . " %i %-12:c%?-12t% s")
+        (todo   . " ")
+        (tags   . " %i %-12:c")
+        (search . " %i %-12:c")))
+
+
+;; CONFIGURACION AGENDA
+(setq org-agenda-custom-commands
+      '(("g" "Get Things Done (GTD)"
+         ((agenda ""
+                  ((org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'deadline))
+                   (org-deadline-warning-days 0)))
+          (todo "NEXT"
+                ((org-agenda-skip-function
+                  '(org-agenda-skip-entry-if 'deadline))
+                 (org-agenda-prefix-format "  %i %-12:c [%e] ")
+                 (org-agenda-overriding-header "\nTasks\n")))
+          (agenda nil
+                  ((org-agenda-entry-types '(:deadline))
+                   (org-agenda-format-date "")
+                   (org-deadline-warning-days 7)
+                   (org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'notregexp "\\* NEXT"))
+                   (org-agenda-overriding-header "\nDeadlines")))
+          (tags-todo "inbox"
+                     ((org-agenda-prefix-format "  %?-12t% s")
+                      (org-agenda-overriding-header "\nInbox\n")))
+          (tags "CLOSED>=\"<today>\""
+                ((org-agenda-overriding-header "\nCompleted today\n")))))))
